b7513573ff0e940001aef2c05780d2f6af3caeca
zohakhanlearning <zohakhanlearning@gmail.com>
2024-12-23 09:24:58 -0700

    Initial commit

--- scripts/terraform_validate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Validating Terraform configuration..."
-terraform validate
-if [[ $? -ne 0 ]]; then
-  echo "Terraform validation failed!"
-  exit 1
-fi
-echo "Terraform validation completed."
