b9f3325e864008535c33eba9239b85aa2222decd
zohakhanlearning <zohakhanlearning@gmail.com>
2024-12-22 22:22:51 -0700

    Initial commit

--- scripts/generate_tfvars.sh
+++ scripts/generate_tfvars.sh
@@ -16,7 +16,7 @@ cat "$CONFIG_FILE"
 # Extract region from the config file
 #AWS_REGION=$(grep '^aws_region:' "$CONFIG_FILE" | awk -F': ' '{print $2}' | tr -d '\"')
 
-AWS_REGION=$(yq -r '.aws_region' "$CONFIG_FILE")
+AWS_REGION=$(yq '.aws_region' "$CONFIG_FILE")
 
 if [[ -z "$AWS_REGION" ]]; then
   echo "Error: AWS Region is not defined in the configuration file."
@@ -25,29 +25,29 @@ fi
 echo "aws_region = \"$AWS_REGION\"" >> "$TFVARS_FILE"
 
 
-yq -r  '.services | keys[]' "$CONFIG_FILE" | while read -r SERVICE; do
+yq   '.services | keys[]' "$CONFIG_FILE" | while read -r SERVICE; do
   case $SERVICE in
     ecs)
-      if yq -e '.services.ecs' "$CONFIG_FILE" > /dev/null 2>&1; then
+      if yq '.services.ecs' "$CONFIG_FILE" > /dev/null 2>&1; then
         echo "Adding ECS configuration..."
-        ECS_CLUSTER_NAME=$(yq -r '.services.ECS.cluster_name' "$CONFIG_FILE")
-        ECS_INSTANCE_TYPE=$(yq -r '.services.ECS.instance_type' "$CONFIG_FILE")
+        ECS_CLUSTER_NAME=$(yq  '.services.ECS.cluster_name' "$CONFIG_FILE")
+        ECS_INSTANCE_TYPE=$(yq  '.services.ECS.instance_type' "$CONFIG_FILE")
         echo "ecs_cluster_name = \"$ECS_CLUSTER_NAME\"" >> "$TFVARS_FILE"
         echo "ecs_instance_type = \"$ECS_INSTANCE_TYPE\"" >> "$TFVARS_FILE"
       fi
       ;;
     s3)
-      if yq -e '.services.s3' "$CONFIG_FILE" > /dev/null 2>&1; then
+      if yq  '.services.s3' "$CONFIG_FILE" > /dev/null 2>&1; then
         echo "Adding S3 configuration..."
-        S3_BUCKET_NAME=$(yq -r '.services.S3.bucket_name' "$CONFIG_FILE")
+        S3_BUCKET_NAME=$(yq  '.services.S3.bucket_name' "$CONFIG_FILE")
         echo "s3_bucket_name = \"$S3_BUCKET_NAME\"" >> "$TFVARS_FILE"
       fi
       ;;
     iam)
       if yq -e '.services.iam' "$CONFIG_FILE" > /dev/null 2>&1; then
         echo "Adding IAM configuration..."
-        IAM_ROLES=$(yq -c '.services.iam.roles' "$CONFIG_FILE")
-        IAM_GROUPS=$(yq -c '.services.iam.groups' "$CONFIG_FILE")
+        IAM_ROLES=$(yq  '.services.iam.roles' "$CONFIG_FILE")
+        IAM_GROUPS=$(yq '.services.iam.groups' "$CONFIG_FILE")
         echo "iam_roles = $IAM_ROLES" >> "$TFVARS_FILE"
         echo "iam_groups = $IAM_GROUPS" >> "$TFVARS_FILE"
       fi
