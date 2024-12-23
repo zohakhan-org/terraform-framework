f40cd755af33a8397860d8f2d5f6bbcdee083c01
zohakhanlearning <zohakhanlearning@gmail.com>
2024-12-22 20:17:48 -0700

    Initial commit

--- scripts/generate_tfvars.sh
+++ scripts/generate_tfvars.sh
@@ -13,7 +13,7 @@ echo "Generating terraform.tfvars..."
 echo "Checking the contents of $CONFIG_FILE..."
 cat "$CONFIG_FILE"
 
-AWS_REGION=$(yq e '.aws_region' "$CONFIG_FILE")
+AWS_REGION=$(yq -r '.aws_region' "$CONFIG_FILE")
 if [[ -z "$AWS_REGION" ]]; then
   echo "Error: aws_region not defined in $CONFIG_FILE"
   exit 1
@@ -27,20 +27,20 @@ yq e  '.services | keys[]' "$CONFIG_FILE" | while read -r SERVICE; do
   case $SERVICE in
     ecs)
       echo "Adding ECS configuration..."
-      ECS_CLUSTER_NAME=$(yq e '.services.ECS.cluster_name' "$CONFIG_FILE")
-      ECS_INSTANCE_TYPE=$(yq e '.services.ECS.instance_type' "$CONFIG_FILE")
+      ECS_CLUSTER_NAME=$(yq -r '.services.ECS.cluster_name' "$CONFIG_FILE")
+      ECS_INSTANCE_TYPE=$(yq -r '.services.ECS.instance_type' "$CONFIG_FILE")
       echo "ecs_cluster_name = \"$ECS_CLUSTER_NAME\"" >> "$TFVARS_FILE"
       echo "ecs_instance_type = \"$ECS_INSTANCE_TYPE\"" >> "$TFVARS_FILE"
       ;;
     s3)
       echo "Adding S3 configuration..."
-      S3_BUCKET_NAME=$(yq e '.services.S3.bucket_name' "$CONFIG_FILE")
+      S3_BUCKET_NAME=$(yq -r '.services.S3.bucket_name' "$CONFIG_FILE")
       echo "s3_bucket_name = \"$S3_BUCKET_NAME\"" >> "$TFVARS_FILE"
       ;;
     iam)
       echo "Adding IAM configuration..."
-      IAM_ROLES=$(yq e '.services.iam.roles' "$CONFIG_FILE")
-      IAM_GROUPS=$(yq e '.services.iam.groups' "$CONFIG_FILE")
+      IAM_ROLES=$(yq -r '.services.iam.roles' "$CONFIG_FILE")
+      IAM_GROUPS=$(yq -r '.services.iam.groups' "$CONFIG_FILE")
       echo "iam_roles = $IAM_ROLES" >> "$TFVARS_FILE"
       echo "iam_groups = $IAM_GROUPS" >> "$TFVARS_FILE"
       ;;
