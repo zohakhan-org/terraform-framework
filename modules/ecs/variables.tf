dbe8613dffcc7d9d8a96d33bec1a55d7b73f317f
zohakhanlearning <zohakhanlearning@gmail.com>
2024-12-23 11:53:45 -0700

    Initial commit

--- modules/ecs/variables.tf
+++ modules/ecs/variables.tf
@@ -1,15 +1,43 @@
-# ecs/variables.tf
-variable "ecs_cluster_name" {
-  description = "The name of the ECS cluster."
+variable "region" {
+  description = "The AWS region to deploy resources"
   type        = string
-}
-
-variable "ecs_instance_type" {
-  description = "The instance type for the ECS instances."
-  type        = string
+  default     = "us-east-1"
 }
 
 variable "ami" {
-  description = "The AMI ID to use for the ECS instance"
+  description = "The AMI ID for ECS instances"
   type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "cluster_name" {
+  description = "Name of the ECS cluster"
+  type        = string
+  default     = "my-ecs-cluster"
+}
+
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "Subnet IDs"
+  type        = list(string)
+}
+
+variable "ecs_task_definition" {
+  description = "The ECS task definition"
+  type        = string
+}
+
+variable "ecs_service_name" {
+  description = "ECS Service name"
+  type        = string
+  default     = "my-ecs-service"
 }
