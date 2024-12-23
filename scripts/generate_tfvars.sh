ec3add025ad9812e6568fcd869e57b8823a5f30e
zohakhanlearning <zohakhanlearning@gmail.com>
2024-12-22 21:06:01 -0700

    Initial commit

--- scripts/generate_tfvars.sh
+++ scripts/generate_tfvars.sh
@@ -23,24 +23,24 @@ fi
 echo "aws_region = \"$AWS_REGION\"" >> "$TFVARS_FILE"
 
 
-jq -r  '.services | keys[]' "$CONFIG_FILE" | while read -r SERVICE; do
+yq -r  '.services | keys[]' "$CONFIG_FILE" | while read -r SERVICE; do
   case $SERVICE in
     ecs)
       echo "Adding ECS configuration..."
-      ECS_CLUSTER_NAME=$(jq -r '.services.ECS.cluster_name' "$CONFIG_FILE")
-      ECS_INSTANCE_TYPE=$(jq -r '.services.ECS.instance_type' "$CONFIG_FILE")
+      ECS_CLUSTER_NAME=$(yq -r '.services.ECS.cluster_name' "$CONFIG_FILE")
+      ECS_INSTANCE_TYPE=$(yq -r '.services.ECS.instance_type' "$CONFIG_FILE")
       echo "ecs_cluster_name = \"$ECS_CLUSTER_NAME\"" >> "$TFVARS_FILE"
       echo "ecs_instance_type = \"$ECS_INSTANCE_TYPE\"" >> "$TFVARS_FILE"
       ;;
     s3)
       echo "Adding S3 configuration..."
-      S3_BUCKET_NAME=$(jq -r '.services.S3.bucket_name' "$CONFIG_FILE")
+      S3_BUCKET_NAME=$(yq -r '.services.S3.bucket_name' "$CONFIG_FILE")
       echo "s3_bucket_name = \"$S3_BUCKET_NAME\"" >> "$TFVARS_FILE"
       ;;
     iam)
       echo "Adding IAM configuration..."
-      IAM_ROLES=$(jq -c '.services.iam.roles' "$CONFIG_FILE")
-      IAM_GROUPS=$(jq -c '.services.iam.groups' "$CONFIG_FILE")
+      IAM_ROLES=$(yq -c '.services.iam.roles' "$CONFIG_FILE")
+      IAM_GROUPS=$(yq -c '.services.iam.groups' "$CONFIG_FILE")
       echo "iam_roles = $IAM_ROLES" >> "$TFVARS_FILE"
       echo "iam_groups = $IAM_GROUPS" >> "$TFVARS_FILE"
       ;;
