7f101d8f6464ddb86d7c226894e70c2c876f61d0
zohakhanlearning <zohakhanlearning@gmail.com>
2024-12-23 00:15:30 -0700

    Initial commit

--- scripts/terraform_init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Initializing Terraform..."
-terraform init
-if [[ $? -ne 0 ]]; then
-  echo "Terraform initialization failed!"
-  exit 1
-fi
-echo "Terraform initialization completed."
