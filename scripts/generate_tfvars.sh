ce857e508fc65e0f3bb3fa50a158c4d95761fba4
zohakhanlearning <zohakhanlearning@gmail.com>
2024-12-22 22:11:35 -0700

    Initial commit

--- scripts/generate_tfvars.sh
+++ scripts/generate_tfvars.sh
@@ -28,28 +28,34 @@ echo "aws_region = \"$AWS_REGION\"" >> "$TFVARS_FILE"
 yq -r  '.services | keys[]' "$CONFIG_FILE" | while read -r SERVICE; do
   case $SERVICE in
     ecs)
-      echo "Adding ECS configuration..."
-      ECS_CLUSTER_NAME=$(yq -r '.services.ECS.cluster_name' "$CONFIG_FILE")
-      ECS_INSTANCE_TYPE=$(yq -r '.services.ECS.instance_type' "$CONFIG_FILE")
-      echo "ecs_cluster_name = \"$ECS_CLUSTER_NAME\"" >> "$TFVARS_FILE"
-      echo "ecs_instance_type = \"$ECS_INSTANCE_TYPE\"" >> "$TFVARS_FILE"
+      if yq -e '.services.ecs' "$CONFIG_FILE" > /dev/null 2>&1; then
+        echo "Adding ECS configuration..."
+        ECS_CLUSTER_NAME=$(yq -r '.services.ECS.cluster_name' "$CONFIG_FILE")
+        ECS_INSTANCE_TYPE=$(yq -r '.services.ECS.instance_type' "$CONFIG_FILE")
+        echo "ecs_cluster_name = \"$ECS_CLUSTER_NAME\"" >> "$TFVARS_FILE"
+        echo "ecs_instance_type = \"$ECS_INSTANCE_TYPE\"" >> "$TFVARS_FILE"
+      fi
       ;;
     s3)
-      echo "Adding S3 configuration..."
-      S3_BUCKET_NAME=$(yq -r '.services.S3.bucket_name' "$CONFIG_FILE")
-      echo "s3_bucket_name = \"$S3_BUCKET_NAME\"" >> "$TFVARS_FILE"
+      if yq -e '.services.s3' "$CONFIG_FILE" > /dev/null 2>&1; then
+        echo "Adding S3 configuration..."
+        S3_BUCKET_NAME=$(yq -r '.services.S3.bucket_name' "$CONFIG_FILE")
+        echo "s3_bucket_name = \"$S3_BUCKET_NAME\"" >> "$TFVARS_FILE"
+      fi
       ;;
     iam)
-      echo "Adding IAM configuration..."
-      IAM_ROLES=$(yq -c '.services.iam.roles' "$CONFIG_FILE")
-      IAM_GROUPS=$(yq -c '.services.iam.groups' "$CONFIG_FILE")
-      echo "iam_roles = $IAM_ROLES" >> "$TFVARS_FILE"
-      echo "iam_groups = $IAM_GROUPS" >> "$TFVARS_FILE"
+      if yq -e '.services.iam' "$CONFIG_FILE" > /dev/null 2>&1; then
+        echo "Adding IAM configuration..."
+        IAM_ROLES=$(yq -c '.services.iam.roles' "$CONFIG_FILE")
+        IAM_GROUPS=$(yq -c '.services.iam.groups' "$CONFIG_FILE")
+        echo "iam_roles = $IAM_ROLES" >> "$TFVARS_FILE"
+        echo "iam_groups = $IAM_GROUPS" >> "$TFVARS_FILE"
+      fi
       ;;
     *)
       echo "Unknown service: $SERVICE"
       ;;
   esac
 done
-cat ./terraform.tfvars
+cat "$TFVARS_FILE"
 echo "Terraform tfvars generated successfully!"
